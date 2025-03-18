5db9427cf6e807ba31cc398f5e5a471708b9e2b8
Alexandr26Starostin <sanchostarostin@gmail.com>
2025-03-18 22:28:19 +0300

    version 8: has good comments

--- my_printf.s
+++ my_printf.s
@@ -9,8 +9,8 @@ extern main     ;main in other file (for ld)
 _start:         ;point of entry in program
     call main  
 
+    mov rdi, rax       ;return rdi;  //rdi == rax
     mov rax, 0x3C      ; end of program
-    xor rdi, rdi       ;return rdi;  //rdi == 0
     syscall
 
 ;---------------------------------------------------------------------------------------------------------
@@ -30,12 +30,9 @@ _start:         ;point of entry in program
 ;
 ;exit:  rax = count of writing symbols
 ;
-;destr: rax = number in interrupt, 
-;             count of writing symbols
-;       rcx = index of count symbols in buffer
-;       rdx = address of buffer, 
-;             count of symbols in buffer in print
-;       rsi = const char* buffer_for_printf (in interrupt)
+;destr: rax = count of writing symbols
+;       rcx = index of count free places in buffer
+;       rdx = address of buffer
 ;       rdi = address on str, 
 ;             stdout
 ;       r10 = old rbp 
@@ -72,7 +69,7 @@ my_printf:
     xor rax, rax    ;rax = 0
     xor rbx, rbx    ;rbx = 0
 
-    mov rcx, [len_buffer]        ;rcx = index of count symbols in buffer
+    mov rcx, [len_buffer]        ;rcx = index of count free places in buffer
     mov rdx, buffer_for_printf   ;rdx = address of buffer
 
     continue_fill_the_buffer:
@@ -86,25 +83,25 @@ my_printf:
         je do_print_argument       ;if (bl == '%') {do_print_argument ();}
 
         ;print_usual_symbol
-        mov [rdx], bl
-        inc rdi
-        inc rdx
-        inc rax
-
+        mov [rdx], bl   ;mov symbol in buffer
+        inc rdi         ;next symbol in str
+        inc rdx         ;nest free place in buffer
+        inc rax         ;+1 writing symbol
         jmp skip_print_argument
 
-        do_print_argument:
+        ;print_argument
+        do_print_argument:  
         call print_argument
-        inc rcx
+        inc rcx                 ;rcx = rcx + 1 - 1 (+1: inc rcx, -1: loop) = rcx (rcx was changed by print_argument)
 
         skip_print_argument:
-        loop continue_fill_the_buffer  ;while (rcx != 0) {continue_fill_the_buffer ();}
+        loop continue_fill_the_buffer  ;while (rcx != 0) {continue_fill_the_buffer ();}  //while free place is in buffer, continue print str
     
-    break_fill_the_buffer:
+    break_fill_the_buffer:  ;buffer is full or see '\0'
 
     ;----------------------------------------------------------------------
 
-    call print_buffer   
+    call print_buffer    ;print symbols in buffer
 
     cmp bl, 0
     je end_of_my_printf      ;if (bl == '\0') {end_print ();}
@@ -129,27 +126,30 @@ my_printf:
 ;---------------------------------------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 ;---------------------------------------------------------------------------------------------------------
 ;                                           print_buffer
+;print buffer with symbols
+;entry: rdi = address on next symbol in str
+;       rax = count of writing symbols
+;       r10 = old rbp
+;       r11 = address to return
+;       rcx = count of free places in buffer
+;       rdx = address of the first free place in buffer
+;       
+;exit:  rdi = address on next symbol in str (not changed)
+;       rax = count of writing symbols (not changed)
+;       r10 = old rbp (not changed)
+;       r11 = address to return (not changed)
+;       rcx = len of buffer: all places in buffer are free
+;       rdx = address on buffer: the first place in buffer is free
+;       
+;destr: rcx = len of buffer
+;       rdx = adress on buffer
 ;
-;entry: 
-;
-;exit:  
-;
-;destr:  
+;use:   rax = number of interrupt
+;       rdx = count of symbols in buffer for print
+;       rsi = address on buffer
+;       rdi = stdout 
 ;---------------------------------------------------------------------------------------------------------
 print_buffer:
 
@@ -166,12 +166,12 @@ print_buffer:
 
     mov rsi, buffer_for_printf   ;rsi == const char* buffer
     mov rdi, 1                   ;rdi == 1 => stdout
-    syscall
+    syscall ;!!! change r10, r11
 
     pop r11
     pop r10
     pop rax
-    pop rdi 
+    pop rdi     ;save rdi, rax, r10, r11
 
     mov rcx, [len_buffer]        ;rcx = index of count symbols in buffer
     mov rdx, buffer_for_printf   ;rdx = address of buffer
@@ -180,287 +180,317 @@ print_buffer:
 ;---------------------------------------------------------------------------------------------------------
 
 
-
-
-
 ;---------------------------------------------------------------------------------------------------------
 ;                                       print_symbols_from_stack
+;value --> (calculus system) --> array of numbers in stack (little end)
+;print this numbers from stack
 ;
+;entry: rcx = count of free places in buffer
+;       r8  = len array of numbers in stack
+;       rdx = address of the next free place in buffer  
+;       rax = r8 (old rax in stack: see end of func)     
+;       rdi = address on next symbol in str (now == <type>)
 ;
-;entry: 
+;exit:  rdx = address of the next free place in buffer  
+;       rcx = count of free places in buffer
+;       rax = count of writing symbols
+;       rdi = address on next symbol in str (now == symbol after <type>)
 ;
-;exit:  
-;
-;destr:    
-;
+;destr: r13 = save address of return for this func 
+;       rcx = count of free places in buffer (new)
+;       rdx = address of the next free place in buffer (new)
+;       r8  = 0 (all numbers in stack are printed)
+;       r9  = old rax
+;       rax = count of writing symbols
+;       rbx = symbol from stack (= bl)
+;       rdi = address on next symbol in str (skip <type>)
 ;---------------------------------------------------------------------------------------------------------
 print_symbols_from_stack:
 
-    pop r13
+    pop r13  ;save address of return for this func
+    ;in stack - all numbers
 
     print_symbol:
 
-        cmp rcx, 0
-        jne continue_print_next_symbol
+        cmp rcx, 0  
+        jne continue_print_next_symbol  ;rcx != 0 --> can put symbol in buffer
 
-        call print_buffer
+        ;rcx == 0 --> buffer doesn't have free places --> print and clear buffer --> can put symbols in buffer
+        call print_buffer  
         continue_print_next_symbol:
 
-        pop rbx
-        mov [rdx], bl            
-        inc rdx
-        dec rcx
-        dec r8
+        pop rbx         ;take symbol from stack to bl
+        mov [rdx], bl   ;put symbol in buffer         
+        inc rdx         ;+1 - next free symbol in buffer
+        dec rcx         ;-1 free place in buffer
+        dec r8          ;-1 number in stack
 
-        cmp r8, 0
-        jne print_symbol
+        cmp r8, 0     
+        jne print_symbol ;r8 != 0 --> print the next number from stack
 
-    pop r9
-    add rax, r9
+    ;r8 == 0 --> all numbers in stack are printed   
 
-    inc rdi ;skip 'type'
+    pop r9      ;now in stack must be old rax
+    add rax, r9 ;new rax = r9 (= old rax) + rax (= r8 in beginning of func)
 
-    push r13
+    inc rdi     ;skip 'type'
+
+    push r13   ;save address of return for this func
 
     ret
 ;---------------------------------------------------------------------------------------------------------
 
 
-
-
-
-
-
-
-
-
-
-
-
 ;---------------------------------------------------------------------------------------------------------
 ;                                       print_argument
 ;print argument of str
+;can print: %c, %d, %b, %o, %x, %s, %%
 ;
-;entry: rdi = address on symbol in str (const char* str)
-;       rdx = buffer for printf        (const char* buffer)
+;entry: rdi = address on next symbol in str (const char* str)
+;       rdx = address on the next free place in buffer for printf (const char* buffer)
 ;       rax = count of writing symbols
-;       rbp = address on arguments
-;       rcx = count of free symbols in buffer
-;       bl  = '%', symbol_after_%
+;       rbp = address on arguments in stack
+;       rcx = count of free places in buffer
+;       bl  = symbol from str (='%')
 ;
-;exit:  
+;exit:  rax = count of writing symbols
+;       rdx = address on the next free place in buffer
+;       rcx = count of free places in buffer
+;       rdi = address on next symbol in str
 ;
-;destr:    
+;destr: bl  = symbol from str
+;       rax = count of writing symbols
+;       rbp = address on the next arguments in stack
+;       rdx = address on the next free place in buffer
+;       rcx = count of free places in buffer
+;       rdi = address on next symbol in str
+;       r8  = count of numbers in stack from value (for %d, %b, %o, %x)
 ;
+;use:   r9  = 10: footing of 10 calculus system (for %d)
+;       r11 =   
+;       r12 = save rdx = address on the next free place in buffer (for %d)
+;           = save rax = value of argument from stack (for %b, %o, %x)
+;           = value % cl = value % r14 = little number from value(for %b, %o, %x)
+;       r13 = save address of return of print_symbols_from_stack (for %d, %b, %o, %x)    
+;       r14 = footing of calculus system (for %b, %o, %x)
+;       r15 = save rcx = count of free places in buffer (for %b, %o, %x)
 ;---------------------------------------------------------------------------------------------------------
 print_argument:
 
     inc rdi  ;skip '%'
 
-    mov bl, [rdi]
-    sub bl, '%'     ;bl = symbol_after_% - 'a'
+    mov bl, [rdi]   ;bl = <type> (=symbol_after_%)
+    sub bl, '%'     ;bl = symbol_after_% - '%'  - shifting for jmp_table
 
-    jmp [type_of_argument + rbx * 8]   ;use jmp_table
+    jmp [type_of_argument + rbx * 8]   ;use jmp_table (rbx = bl = shifting for jmp_table)
  
-    type_c:                ;%c
-        mov rbx, [rbp]     ;rpb = address on free argument (== symbol)
-        mov [rdx], bl      ;print one symbol
+    ;----------------------------------------------------------------------
 
-        add rbp, 8         ;rbp = address on the next argument
-        inc rdi            
-        inc rdx
-        dec rcx
-        inc rax
+    type_c:                ;%c
+        mov rbx, [rbp]     ;rpb = address on next argument (== symbol)
+        mov [rdx], bl      ;print one symbol (put it in buffer)
+
+        add rbp, 8    ;rbp = address on the next argument
+        inc rdi       ;+1 - the next symbol in str
+        inc rdx       ;+1 - the next free place in buffer        
+        dec rcx       ;-1 count of free places in buffer
+        inc rax       ;+1 count of writing symbols
         
-        jmp end_print_argument
+        jmp end_print_argument ;end of func
 
     ;----------------------------------------------------------------------
 
     type_d:   ;%d
-        push r13
-        push r12
+        push r13 
+        push r12   ;must save r12, r13
 
-        push rax
+        push rax   ;save rax for end func, when func will count new rax = count of writing symbols
         mov r12, rdx  ;save rax, rdx for div
 
-        mov rax, [rbp]     ;rpb = address on free argument (== int_10)
+        mov rax, [rbp]     ;rpb = address on next argument (== int_10)
         add rbp, 8         ;rbp = address on the next argument
 
-        xor r8, r8  ;r8 = 0 (count of symbols in number_10)
+        xor r8, r8  ;r8 = 0 (count of numbers in value symbols for number_10)
 
-        count_next_symbol_in_number_10:
+        count_next_symbol_in_number_10:  ;take number from value and put it in stack
 
-        cqo
-        mov r9, 10
-        div r9   ;rax = rax // 10
-                 ;rdx = rax %  10
-        inc r8
-        add rdx, '0'
-        push rdx
+        cqo          ;rax --> rdx:rax
+        mov r9, 10   ;r9  = footing of 10 calculus system 
+        div r9       ;rax = rax // 10
+                     ;rdx = rax %  10
+        inc r8       ;+1 count of numbers in stack
+        add rdx, '0' ;numbers --> 'numbers' (ascii)
+        push rdx     ;put number in stack
 
         cmp rax, 0
-        jne count_next_symbol_in_number_10
+        jne count_next_symbol_in_number_10 ;rax != 0 --> can put number from rax in stack
 
-        mov rax, r8
-        mov rdx, r12
+        ;all numbers from value in stack
+        mov rax, r8    ;rax = count of numbers in stack (count of writing symbols will be counted in print_symbols_from_stack)
+        mov rdx, r12   ;rdx = address on the next free place in buffer 
 
         ;---------------------------------------------------------------
 
         call print_symbols_from_stack        
 
         pop r12
-        pop r13
+        pop r13   ;save r12, r13
 
-        jmp end_print_argument
+        jmp end_print_argument  ;end of print_argument
 
     ;----------------------------------------------------------------------
     
     type_b:    ;%b
-        push r14
-        mov r14, 1
+        push r14    ;must save r14
+        mov r14, 1  ;r14 = footing of calculus system = 2^1 = 2 (for shiftings)
         jmp continue_print_argument_with_footing
 
     type_o:    ;%o
-        push r14
-        mov r14, 3
+        push r14    ;must save r14
+        mov r14, 3  ;r14 = footing of calculus system = 2^3 = 8 (for shiftings)
         jmp continue_print_argument_with_footing
 
     type_x:    ;%x
-        push r14
-        mov r14, 4
+        push r14    ;must save r14
+        mov r14, 4  ;r14 = footing of calculus system = 2^4 = 16 (for shiftings)
         jmp continue_print_argument_with_footing
 
     continue_print_argument_with_footing:
     push r13
     push r12
-    push r15
+    push r15        ;must save r13, r12, r15
 
-    push rax
+    push rax        ;save rax for end func, when func will count new rax = count of writing symbols
 
-    mov rax, [rbp]     ;rpb = address on free argument (== int_10)
+    mov rax, [rbp]     ;rpb = address on free argument (== int_r14)
+                       ;rax = value of argument from stack  
     add rbp, 8         ;rbp = address on the next argument
 
-    xor r8, r8  ;r8 = 0 (count of symbols in number_10)
+    xor r8, r8  ;r8 = 0 (count of numbers in value symbols for number_r14)
 
-    mov r15, rcx
-    mov rcx, r14
+    mov r15, rcx    ;save rcx = count of free places in buffer
+    mov rcx, r14    ;rcx = cl = r14 = footing of calculus system
+
     count_next_symbol_in_number:
+    mov r12, rax   ;save rax
 
-    nop
-    nop
-    nop
+    shr rax, cl  
+    shl rax, cl    ;rax = (rax >> cl) << cl
+                   
+    sub r12, rax   ;r12 = r12 - rax 
+                   ;r12 = value % cl = little number from value
 
-    mov r12, rax
-    shr rax, cl    ;rax = rax // cl
-    shl rax, cl
-    sub r12, rax  ;r12 = rax %  cl
-    shr rax, cl
+    shr rax, cl    ;rax = rax >> cl
+                   ;rax = value // cl
 
-    cmp r12, 10
-    js have_number
+    cmp r12, 10   
+    js have_number    ;r12 < 10 --> r2 - number
 
     ;have_latter
     sub r12, 10
-    add r12, 'A'
+    add r12, 'A'    ;number --> 'latter' (acscii)
     jmp write_r12_in_stack
 
     have_number:
-    add r12, '0'
+    add r12, '0'    ;number --> 'number' (ascii)
 
+    ;pur number in stack
     write_r12_in_stack:
 
-    inc r8
-    push r12
+    inc r8    ;+1 count numbers in stack
+    push r12  ;pur number in stack
 
-    cmp rax, 0
-    jne count_next_symbol_in_number
+    cmp rax, 0 
+    jne count_next_symbol_in_number  ;rax != 0 --> can put number from rax in stack
 
-    mov rcx, r15
+    ;all numbers from value in stack
+    mov rcx, r15   ;rcx = r15 = count of free places in buffer
+    mov rax, r8    ;rax = count of numbers in stack (count of writing symbols will be counted in print_symbols_from_stack)
 
-    mov rax, r8
-
-    ;---------------------------------------------------------------
+    ;--------------------------------------------------------------- 
 
     call print_symbols_from_stack
     
     pop r15
     pop r12
     pop r13
-    pop r14
+    pop r14    ;save r15, r12, r13, r14
 
-    jmp end_print_argument
+    jmp end_print_argument ;end of print_argument
     ;----------------------------------------------------------------------
 
-    type_s:
+    type_s:     ;%s
 
-        push rdi
+        push rdi  ;save address on next symbol in "main" str
 
         mov rdi, [rbp]     ;rpb = address on free argument (== const char*)
+                           ;rdi = address on "argument" str
         add rbp, 8         ;rbp = address on the next argument
 
         continue_print_str:
 
-            mov bl, [rdi]     
+            mov bl, [rdi]       ;bl = symbol from "argument" str
 
             cmp bl, 0   
-            je break_print_str   
+            je break_print_str  ;bl == 0 --> end print "argument" str
 
             ;print_usual_symbol
-            mov [rdx], bl
-            inc rdi
-            inc rdx
-            inc rax
+            mov [rdx], bl      ;put symbol from "argument" str on free place in buffer
+            inc rdi            ;+1 - next symbol in "argument" str  
+            inc rdx            ;+1 - next free place in buffer
+            inc rax            ;+1 - count of writing symbols
 
-            loop continue_print_str  
+            loop continue_print_str  ;rcx -= 1 (-1 count of free places in buffer)
 
         ;----------------------------------------------------------------------
 
-        call print_buffer   
+        ;rcx == 0 && bl != '\0' --> buffer is full
+        call print_buffer          ;print and clear buffer
+        jmp continue_print_str     ;fill buffer again
 
-        jmp continue_print_str      ;fill buffer again
+        break_print_str:       ;end print "argument" str
+        pop rdi                ;rdi = address on next symbol in "main" str
+        inc rdi                ;skip 's'
 
-        break_print_str:
-        pop rdi
-        inc rdi    ;skip 's'
-        jmp end_print_argument
+        jmp end_print_argument ;end print_argument
     ;----------------------------------------------------------------------
 
-    type_percent:
-        mov bl, '%'
+    type_percent:     ;%%
+        mov bl, '%'   ;bl = '%'
 
-        mov [rdx], bl
-        inc rdi     ;skip '%'
-        inc rdx
-        inc rax
-        dec rcx
+        mov [rdx], bl   ;print '%'
+        inc rdi         ;skip '%'
+        inc rdx         ;+1 - next free place in buffer
+        inc rax         ;+1 - count of writing symbols
+        dec rcx         ;-1 - count of free places in buffer
 
-        jmp end_print_argument
+        jmp end_print_argument  ;end print_argument
     ;----------------------------------------------------------------------
 
     end_print_argument:
 
-    default_:
+    default_:  ;default
 
     ret
 ;---------------------------------------------------------------------------------------------------------
 
 section .data   ;has data
-len_buffer dq 16
-buffer_for_printf: times 16 db 0
+len_buffer dq 16   ;len buffer == max count of free places in buffer
+buffer_for_printf: times 16 db 0  ;buffer for symbols
 
-align 8    ;8 bytes
-type_of_argument:     ;jmp_table
-    dq   type_percent
+align 8    ;8 bytes between labels
+type_of_argument:       ;jmp_table
+    dq   type_percent   ;%%
     times 59 dq default_
-    dq   type_b
-    dq   type_c
-    dq   type_d
+    dq   type_b         ;%b
+    dq   type_c         ;%c
+    dq   type_d         ;%d
     times 10 dq default_
-    dq   type_o
+    dq   type_o         ;%o
     times 3 dq default_
-    dq   type_s
+    dq   type_s         ;%s
     times 4 dq default_
-    dq   type_x
+    dq   type_x         ;%x
     times 2 dq default_
     
 
